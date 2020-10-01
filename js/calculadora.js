1c870ce16086a1c93dfc27f98cd66faa33a3cb8b
jhenifer <jhenifermarqs@gmail.com>
2020-09-30 21:59:28 -0300

    calculadora feita

---
+++ js/calculadora.js
@@ -0,0 +1,9 @@
+var n1 = prompt("Digite o valor desejado:");
+var n2 = prompt("Digite o segundo valor:");
+var numero = parseFloat(n1);
+var numero2 = parseFloat(n2);
+var soma = numero + numero2;
+var resultado = parseInt(soma);
+console.log(resultado);
+
+alert(soma);
